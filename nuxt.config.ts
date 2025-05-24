75974ef902cb54f8061f0714cfd119ca3d56d371
Alit Indrawan <alitindrawan71@gmail.com>
2025-05-24 13:17:38 +0800

    refactor: update app head configuration and add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  app: {
+    head: {
+      title: 'Pokemon TCG Pocket',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ]
+    }
+  },
   devServer: {
     port: 3030
   },
